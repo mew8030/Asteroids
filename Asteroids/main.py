ae79692ac309b1ebebb3812989592e86578de0e5
mew8030 <mew8030@proton.me>
2025-11-30 14:55:49 -0500

    A5: Added the asteroidfield functionality that populates the space with circular asteroids

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,5 +1,7 @@
 import pygame
+from asteroidfield import AsteroidField
 from player import Player
+from asteroid import Asteroid
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
@@ -11,12 +13,19 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.vernum}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     t = pygame.time.Clock()
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+    field = AsteroidField()
 
     while(True):
         log_state()
@@ -24,7 +33,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+
+        updatable.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = t.tick(60)/1000
 
